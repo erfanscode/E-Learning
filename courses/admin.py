e8c1ba56eab5cb171159732dc64f4d7afb303f6c
erfanscode <codeerfan@gmail.com>
2025-02-07 04:43:14 +0330

    modified files based on flake8

--- courses/admin.py
+++ courses/admin.py
@@ -4,24 +4,30 @@ from .models import Subject, Course, Module
 
 # ---------- Inline Classes ---------- #
 class ModuleInline(admin.StackedInline):
-  # The ModuleInline class is a stacked inline for the Module model
-  model = Module
-  extra = 1
+    ''' The ModuleInline class is a stacked inline for the Module model '''
+    model = Module
+    extra = 1
 
 
 # ---------- Admin Classes ---------- #
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
-  # The SubjectAdmin class customizes the admin interface for the Subject model
-  list_display = ['title', 'slug']
-  prepopulated_fields = {'slug': ('title',)}
+    '''
+      The SubjectAdmin class customizes
+      the admin interface for the Subject model
+    '''
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
 
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-  # The CourseAdmin class customizes the admin interface for the Course model
-  list_display = ['title', 'subject', 'created']
-  list_filter = ['created', 'subject']
-  search_fields = ['title', 'overview']
-  prepopulated_fields = {'slug': ('title',)}
-  inlines = [ModuleInline]
+    '''
+      The CourseAdmin class customizes
+      the admin interface for the Course model
+    '''
+    list_display = ['title', 'subject', 'created']
+    list_filter = ['created', 'subject']
+    search_fields = ['title', 'overview']
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [ModuleInline]
--- courses/models.py
+++ courses/models.py
@@ -3,54 +3,54 @@ from django.contrib.auth.models import User
 
 
 class Subject(models.Model):
-  # The Subject model represents a subject that students can study
-  title = models.CharField(max_length=200, verbose_name='عنوان')
-  slug = models.SlugField(max_length=225, unique=True, verbose_name='اسلاگ')
+    ''' The Subject model represents a subject that students can study '''
+    title = models.CharField(max_length=200, verbose_name='عنوان')
+    slug = models.SlugField(max_length=225, unique=True, verbose_name='اسلاگ')
 
-  class Meta:
-    ordering = ['title']
+    class Meta:
+        ordering = ['title']
 
-  def __str__(self):
-    return self.title
-  
+    def __str__(self):
+        return self.title
+
 
 class Course(models.Model):
-  # The Course model represents a course that students can take
-  owner = models.ForeignKey(
-    User, related_name='courses_created',
-    on_delete=models.CASCADE,
-    verbose_name='سازنده'
-  )
-  subject = models.ForeignKey(
-    Subject,
-    related_name='courses',
-    on_delete=models.CASCADE,
-    verbose_name='موضوع'
-  )
-  
-  title = models.CharField(max_length=200, verbose_name='عنوان')
-  slug = models.SlugField(max_length=225, unique=True, verbose_name='اسلاگ')
+    ''' The Course model represents a course that students can take '''
+    owner = models.ForeignKey(
+      User, related_name='courses_created',
+      on_delete=models.CASCADE,
+      verbose_name='سازنده'
+    )
+    subject = models.ForeignKey(
+      Subject,
+      related_name='courses',
+      on_delete=models.CASCADE,
+      verbose_name='موضوع'
+    )
 
-  overview = models.TextField(verbose_name='توضیح')
-  created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200, verbose_name='عنوان')
+    slug = models.SlugField(max_length=225, unique=True, verbose_name='اسلاگ')
 
-  class Meta:
-    ordering = ['-created']
+    overview = models.TextField(verbose_name='توضیح')
+    created = models.DateTimeField(auto_now_add=True)
 
-  def __str__(self):
-    return self.title
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
 
 
 class Module(models.Model):
-  # The Module model represents a module that belongs to a course
-  course = models.ForeignKey(
-    Course,
-    related_name='modules',
-    on_delete=models.CASCADE,
-    verbose_name='دوره'
-  )
-  title = models.CharField(max_length=200, verbose_name='عنوان')
-  description = models.TextField(blank=True, verbose_name='توضیح')
+    ''' The Module model represents a module that belongs to a course '''
+    course = models.ForeignKey(
+      Course,
+      related_name='modules',
+      on_delete=models.CASCADE,
+      verbose_name='دوره'
+    )
+    title = models.CharField(max_length=200, verbose_name='عنوان')
+    description = models.TextField(blank=True, verbose_name='توضیح')
 
-  def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
