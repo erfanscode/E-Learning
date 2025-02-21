0b8cbae7bb5e972a0646f7ebbddba55a321872ff
erfanscode <codeerfan@gmail.com>
2025-02-21 05:19:28 +0330

    adding url for managing modules and their contents

--- courses/urls.py
+++ courses/urls.py
@@ -21,5 +21,7 @@ urlpatterns = [
          views.ContentCreateUpdateView.as_view(),
          name='module_content_update'),
     path('module/<int:id>/delete/',
-         views.ContentDeleteView.as_view(), name='module_content_delete')
+         views.ContentDeleteView.as_view(), name='module_content_delete'),
+    path('module/<int:module_id>/',
+         views.ModuleContentListView.as_view(), name='module_content_list')
 ]
