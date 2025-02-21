c429a2440dd12e47a97d969e4893c9b15f17447b
erfanscode <codeerfan@gmail.com>
2025-02-22 02:41:10 +0330

    fix a bug in update ContentCreateUpdateView url

--- courses/urls.py
+++ courses/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('module/<int:module_id>/content/<model_name>/create/',
          views.ContentCreateUpdateView.as_view(),
          name='module_content_create'),
-    path('module/<int:module_id>/content/<model_name>/id/',
+    path('module/<int:module_id>/content/<model_name>/<int:id>/',
          views.ContentCreateUpdateView.as_view(),
          name='module_content_update'),
     path('module/<int:id>/delete/',
