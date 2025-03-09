469ff17dfc1c20a2b1c43310519f98ba1ec28265
erfanscode <codeerfan@gmail.com>
2025-03-10 02:22:26 +0330

    adding view for List and Detail enrolling students course

--- students/views.py
+++ students/views.py
@@ -2,12 +2,15 @@ from django.urls import reverse_lazy
 
 from django.views.generic.edit import CreateView
 from django.views.generic.edit import FormView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import CourseEnrollForm
+from courses.models import Course
 
 
 class StudentRegistrationView(CreateView):
@@ -42,3 +45,40 @@ class StudentEnrollCourseView(LoginRequiredMixin, FormView):
             'student_course_detail',
             args=[self.course.id]
         )
+
+
+class StudentCourseListView(LoginRequiredMixin, ListView):
+    """ View for listing courses for a student """
+    model = Course
+    template_name = 'students/course/list.html'
+
+    def get_queryset(self):
+        # Get courses enrolled by the user
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+
+class StudentCourseDetailView(DetailView):
+    """ View for displaying details of a student's course """
+    model = Course
+    template_name = 'students/course/detail.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+    
+    def get_context_data(self, **kwargs):
+        # Add additional context data to the template
+        context = super().get_context_data(**kwargs)
+        # Get the course object
+        course = self.get_object()
+        if 'module_id' in self.kwargs:
+            # Filter by module
+            context['module'] = course.modules.get(
+                id=self.kwargs['module_id']
+            )
+        else:
+            # Get the first module
+            context['module'] = course.modules.all()[0]
+        return context
+
