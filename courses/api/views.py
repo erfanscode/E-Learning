123da9f0f87c3171ceefb6d9fd9cd096b6c6ae21
erfanscode <codeerfan@gmail.com>
2025-03-13 02:59:45 +0330

    adding viewsets for course model for show list and detail

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import get_object_or_404
 
 from rest_framework import generics
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from courses.models import Subject, Course
-from courses.api.serializers import SubjectSerializer
-
+from courses.api.serializers import (
+  SubjectSerializer,
+  CourseSerializer
+)
 
 class SubjectListView(generics.ListAPIView):
     """ List all subjects """
@@ -31,3 +34,9 @@ class CourseEnrollView(APIView):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    """ List all courses, or retrieve one by its slug """
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
