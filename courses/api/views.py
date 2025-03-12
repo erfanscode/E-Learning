ed35e420b2f7fa7ca048ad710f8284db94bef392
erfanscode <codeerfan@gmail.com>
2025-03-13 03:14:18 +0330

    adding additional action to CourseViewset for enroll courses and remove CourseEnrollView class and course_enroll url

--- courses/api/views.py
+++ courses/api/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -26,18 +27,19 @@ class SubjectDetailView(generics.RetrieveAPIView):
     serializer_class = SubjectSerializer
 
 
-class CourseEnrollView(APIView):
-    """ Enroll a user in a course """
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request, pk, format=None):
-        course = get_object_or_404(Course, pk=pk)
-        course.students.add(request.user)
-        return Response({'enrolled': True})
-
-
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     """ List all courses, or retrieve one by its slug """
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    # Action to enroll a user in a course
+    @action(
+        detail=True,
+        methods=['post'],
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated]
+    )
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
