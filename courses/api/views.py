f8ac1f65dfbd8876436a57d3ba4d4b37d721ffb3
erfanscode <codeerfan@gmail.com>
2025-03-13 03:15:13 +0330

    flake8

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,8 +1,5 @@
-from django.shortcuts import get_object_or_404
-
 from rest_framework import generics
 from rest_framework import viewsets
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.authentication import BasicAuthentication
