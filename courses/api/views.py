58c999d2f740e7d05a509110eb780c995224ca59
erfanscode <codeerfan@gmail.com>
2025-03-12 05:53:08 +0330

    building custom api view and make url for enrolling courses

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,10 @@
-from rest_framework import generics
+from django.shortcuts import get_object_or_404
 
-from courses.models import Subject
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from courses.models import Subject, Course
 from courses.api.serializers import SubjectSerializer
 
 
@@ -14,3 +18,11 @@ class SubjectDetailView(generics.RetrieveAPIView):
     """ Retrieve a subject by its slug """
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+
+class CourseEnrollView(APIView):
+    """ Enroll a user in a course """
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
