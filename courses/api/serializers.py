1bc25fb1ffc8dea5138572d8301959fa60488d76
erfanscode <codeerfan@gmail.com>
2025-03-13 04:20:15 +0330

    Add nested course content serializers for detailed API responses

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 
-from courses.models import Subject, Course, Module
+from courses.models import (
+  Subject,
+  Course,
+  Module,
+  Content
+)
 
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -25,3 +30,37 @@ class CourseSerializer(serializers.ModelSerializer):
         model = Course
         fields = ['id', 'subject', 'title', 'slug',
                   'overview', 'created', 'owner', 'modules']
+
+
+class ItemRelatedField(serializers.RelatedField):
+    """ A custom field to return the URL of the item """
+    def to_representation(self, value):
+        return value.render()
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    """ Serializer for the Content model """
+    item = ItemRelatedField(read_only=True)
+
+    class Meta:
+        model = Content
+        fields = ['order', 'item']
+
+
+class ModuleWithContentsSerializer(serializers.ModelSerializer):
+    """ Serializer for the Module model with its contents """
+    contents = ContentSerializer(many=True)
+
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description', 'contents']
+
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+    """ Serializer for the Course model with its contents """
+    modules = ModuleWithContentsSerializer(many=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug', 'overview',
+                  'created', 'owner', 'modules']
