5ab8648f39bbd4b763ff2f466c9915b59f8569b8
erfanscode <codeerfan@gmail.com>
2025-03-13 04:22:09 +0330

    adding action to retrieve the contents of a course

--- courses/api/views.py
+++ courses/api/views.py
@@ -6,9 +6,11 @@ from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from courses.models import Subject, Course
+from courses.api.permissions import IsEnrolled
 from courses.api.serializers import (
   SubjectSerializer,
-  CourseSerializer
+  CourseSerializer,
+  CourseWithContentsSerializer
 )
 
 
@@ -40,3 +42,14 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+    # Action to retrieve the contents of a course
+    @action(
+        detail=True,
+        methods=['get'],
+        serializer_class=CourseWithContentsSerializer,
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated, IsEnrolled]
+    )
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
