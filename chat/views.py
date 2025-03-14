42af1b2eb7dad2cba37325c2bd81e70bd027b627
erfanscode <codeerfan@gmail.com>
2025-03-14 05:24:55 +0330

    fix flake8 errors

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
 
@@ -8,7 +8,7 @@ def course_chat_room(request, course_id):
     try:
         # Retrieve course with given id joined by the current user
         course = request.user.courses_joined.get(id=course_id)
-    except:
+    except request.user.courses_joined.model.DoesNotExist:
         # User is not a student of the course or course does not exist
         return HttpResponseForbidden()
     return render(request, 'chat/room.html', {'course': course})
