45556fb5a8fa47f774e5cc390633e469fb291e3e
erfanscode <codeerfan@gmail.com>
2025-03-15 04:30:27 +0330

    adding websocket to asgi applications

--- education/asgi.py
+++ education/asgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'education.settings')
 
@@ -18,4 +21,7 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
 })
