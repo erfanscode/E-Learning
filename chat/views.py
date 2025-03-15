6961882fb28b75e17d0141d23147ce4ae3962e12
erfanscode <codeerfan@gmail.com>
2025-03-15 04:26:19 +0330

    updating course_chat_room view

--- chat/views.py
+++ chat/views.py
@@ -2,13 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
 
+from courses.models import Course
 
 @login_required
 def course_chat_room(request, course_id):
     try:
         # Retrieve course with given id joined by the current user
         course = request.user.courses_joined.get(id=course_id)
-    except request.user.courses_joined.model.DoesNotExist:
+    except Course.DoesNotExist:
         # User is not a student of the course or course does not exist
         return HttpResponseForbidden()
-    return render(request, 'chat/room.html', {'course': course})
+    latest_messages = course.chat_messages.select_related('user').order_by('-id')[:5]
+    latest_messages = reversed(latest_messages)
+    return render(
+        request,
+        'chat/room.html',
+        {'course': course, 'latest_messages': latest_messages}
+    )
